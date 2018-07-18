cd8be5cc29c8ebbfe7c9ba48d1c3ac441b40db93
Roman Budnyk <r.m.budnik@gmail.com>
2018-07-18 13:26:56 +0300

    added SuppressWarnings

--- src/main/java/com/codespace/myLibraryConnector/LibraryActivities.java
+++ src/main/java/com/codespace/myLibraryConnector/LibraryActivities.java
@@ -26,6 +26,7 @@ public class LibraryActivities {
         }
     }
 
+    @SuppressWarnings("unchecked")
     private void readFromTheFile() {
         try (ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(bookFile))) {
             bookList = (List<Book>) objectInputStream.readObject();
