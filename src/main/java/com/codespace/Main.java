20c61adec1eaca40c855600faaf4ece77a7ef3db
Roman Budnyk <r.m.budnik@gmail.com>
2018-07-27 21:05:41 +0300

    test

--- src/main/java/com/codespace/Main.java
+++ src/main/java/com/codespace/Main.java
@@ -3,8 +3,6 @@ package com.codespace;
 import com.codespace.myLibraryWorkflow.Controller;
 import org.apache.log4j.Logger;
 
-
-
 public class Main {
     private static final Logger logger = Logger.getLogger(Main.class);
     public static void main(String[] args) {
